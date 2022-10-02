ce18531766f3fdaac188c8fb4069a0eedff99ab9
Kovacs Sandor <sada93@citromail.hu>
2022-10-02 19:14:04 +0200

    feature/TL-16 review fixed

--- src/Components/TodoList/List/List.js
+++ src/Components/TodoList/List/List.js
@@ -1,23 +1,21 @@
 import ListItem from "./ListItem";
 
 const List = ({ todos, setTodos }) => {
-  if (todos.length > 0) {
-    return (
-      <div className="wrapper">
-        {todos.map((todo) => (
-          <ListItem
-            title={todo.title}
-            date={todo.date}
-            status={todo.status}
-            id={todo.id}
-            key={todo.id}
-            setTodos={setTodos}
-            todos={todos}
-          />
-        ))}
-      </div>
-    );
-  }
+  return (
+    <div className="wrapper">
+      {todos.map((todo) => (
+        <ListItem
+          title={todo.title}
+          date={todo.date}
+          status={todo.status}
+          id={todo.id}
+          key={todo.id}
+          setTodos={setTodos}
+          todos={todos}
+        />
+      ))}
+    </div>
+  );
 };
 
 export default List;
